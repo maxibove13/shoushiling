83d1c2b162854bbac044d0c91e7fd0f48ada02c6
maxibove13 <maxibove13@gmail.com>
2021-02-04 10:07:50 -0300

    logo and web title added

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -5,6 +5,10 @@ import { Button } from "../../components";
 import "./styles.scss";
 
 const Login = () => {
+  const fetchEvents = async () => {
+    const res = await fetch("");
+  };
+
   return (
     <div>
       <h2>Shoushiling</h2>
